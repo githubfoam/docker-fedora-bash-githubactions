6bdac6434b235d604331530e1a21a0c2aad672c0
Your Name <you@example.com>
2021-11-19 10:36:33 +0300

    podman

---
+++ scripts/deploy-manual-build-apache-podman.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+# set -eox pipefail #safety for script
+
+echo "========================================================================================="
+mkdir demo-docker && cd demo-docker
+
+source /etc/os-release
+# . /etc/os-release
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
+
+
+echo "========================================================================================="
